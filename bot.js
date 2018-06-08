a2ef3e1117bcea3be19cb033ecb34c356895e270
purecookie <40073195+purecookie@users.noreply.github.com>
2018-06-08 17:12:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,15 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'bypass site') {
-    	message.reply('The Website Is www.e621-is-great.rf.gd');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
